0edbe71e92f8884b457be4c42e239d7f0254200e
Camden Van Sciver <camdenvs02@gmail.com>
2023-06-24 15:22:48 -0700

    quick bugfix

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import { useMutation } from '@apollo/client';
 import { CREATE_USER } from "../utils/mutations"
 import Auth from '../utils/auth';
-import { Link } from 'react-router-dom'
 import {
     Container,
     Input,
