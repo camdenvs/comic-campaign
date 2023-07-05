09c10f601065829dc579f663bb9aee616069f041
Camden Van Sciver <camdenvs02@gmail.com>
2023-07-05 00:06:50 -0700

    made changes to the menu for the mobile view

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link as RouteLink } from 'react-router-dom'
 import { Flex, Link, UnorderedList, ListItem, Image, Menu, MenuButton, MenuList, MenuItem, Hide, Show, Box } from '@chakra-ui/react'
-
+import { FaBars } from 'react-icons/fa'
 import Auth from '../../utils/auth'
 import ShoppingCart from './ShoppingCart'
 
@@ -13,20 +13,20 @@ const Header = () => {
         event.preventDefault()
         Auth.logout()
     }
-    
+
     const userId = Auth.loggedIn() ? Auth.getProfile().data._id : undefined
-    const { loading, data } = useQuery(QUERY_CART, {variables: { userId: userId }})
+    const { loading, data } = useQuery(QUERY_CART, { variables: { userId: userId } })
     const cart = data?.cart || {}
 
     return (
         <header>
             <Flex flexDirection='row' border='1px' borderColor='gray.200' boxShadow={'md'} bgColor={'white'} h={'15vh'} justifyContent={'space-around'}>
                 <Box pos='absolute' top='15px' right={'0.2%'} color={'#212C42'}>
-                {Auth.loggedIn() ? (
-                    <ShoppingCart cart={cart} loading={loading}/>
-                ) : (
-                    <ShoppingCart />
-                )}
+                    {Auth.loggedIn() ? (
+                        <ShoppingCart cart={cart} loading={loading} />
+                    ) : (
+                        <ShoppingCart />
+                    )}
                 </Box>
 
                 <Flex my='auto'>
@@ -78,17 +78,13 @@ const Header = () => {
                         </UnorderedList>
                     </Show>
                     <Hide above='md'>
-                        <Menu>
-                            <MenuButton>
-                                <Image
-                                    src='/assets/images/Logo_AllCAPScomics_Circle.webp'
-                                    alt='all caps logo'
-                                    w={75}
-                                />
+                        <Menu pos='fixed' top='10px' left={'15px'} color={'#212C42'}>
+                            <MenuButton pos='fixed' top='5vh' left={'15px'} color={'#212C42'}>
+                                <FaBars size={'20px'}/>
                             </MenuButton>
                             <MenuList zIndex={999}>
                                 <MenuItem as={RouteLink} to='/' mr={{ md: '5', lg: '10' }}>Home</MenuItem>
-                                <MenuItem as={RouteLink} to='/about' mr={{ md: '5', lg: '10' }}>About</MenuItem>
+                                {/* <MenuItem as={RouteLink} to='/about' mr={{ md: '5', lg: '10' }}>About</MenuItem> */}
                                 <MenuItem as={RouteLink} to='/store' mr={{ md: '5', lg: '10' }}>Store</MenuItem>
                                 <MenuItem as={RouteLink} to='/campaigns' mr={{ md: '5', lg: '10' }}>Campaigns</MenuItem>
                                 <MenuItem as={RouteLink} to='/news' mr={{ md: '5', lg: '10' }}>News</MenuItem>
@@ -97,10 +93,17 @@ const Header = () => {
                                         <MenuItem onClick={logout} textDecoration={'none'} mr={{ md: 'none', xl: '190px' }}>Logout</MenuItem>
                                     </>
                                 ) : (
-                                    <MenuItem mr={{ md: 'none', xl: '190px' }}as={RouteLink} to='/login'>Login</MenuItem>
+                                    <MenuItem mr={{ md: 'none', xl: '190px' }} as={RouteLink} to='/login'>Login</MenuItem>
                                 )}
                             </MenuList>
                         </Menu>
+                        <Box>
+                            <Image
+                            src='/assets/images/Logo_AllCAPScomics_Circle.webp'
+                            alt='all caps logo'
+                            w={'75px'}
+                        />
+                        </Box>
                     </Hide>
                 </Flex>
             </Flex>
